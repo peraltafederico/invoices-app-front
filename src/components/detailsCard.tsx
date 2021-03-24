718a6cdb8020c18fdb392610092d05c4f06e049b
Peralta Federico <peralta.federico.manuel@gmail.com>
2021-03-24 00:52:18 -0300

    add grid component

--- src/components/detailsCard.tsx
+++ src/components/detailsCard.tsx
@@ -1,22 +1,34 @@
 import styled from '@emotion/styled'
 import React from 'react'
 import Card from './card'
+import Grid from './grid'
 import InvoiceId from './invoiceId'
 import Text from './text'
 
 interface Props {}
 
-export const StyledType = styled(Text)`
+export const StyledInvoiceType = styled(Text)`
   margin-top: ${(props) => props.theme.space[2]};
+  margin-bottom: 0.1rem;
+`
+
+export const StyledAddressContainer = styled.div`
+  margin: 3rem 0;
 `
 
 export const DetailsCard = (props: Props) => {
   return (
     <Card>
       <InvoiceId id="XM9141" />
-      <StyledType variant="body2" muted>
+      <StyledInvoiceType variant="body2" muted>
         Graphic Design
-      </StyledType>
+      </StyledInvoiceType>
+      <StyledAddressContainer>
+        <Text muted>19 Union Terrace</Text>
+        <Text muted>London</Text>
+        <Text muted>E1 3EZ</Text>
+        <Text muted>United Kingdom</Text>
+      </StyledAddressContainer>
     </Card>
   )
 }
---
+++ src/components/grid.tsx
@@ -0,0 +1,80 @@
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+import React from 'react'
+
+type Props = {
+  container?: boolean
+  gap?: string
+  rowGap?: string
+  span?: number
+  children?: React.ReactNode
+}
+
+const StyledContainer = styled.div<{
+  gap?: string
+  rowGap?: string
+  container?: boolean
+  span?: number
+}>`
+  ${(props) =>
+    props.container &&
+    props.gap &&
+    css`
+      margin-left: -${props.gap};
+    `}
+
+  ${(props) =>
+    props.container &&
+    props.rowGap &&
+    css`
+      margin-top: -${props.gap};
+    `}
+
+    ${(props) =>
+    !props.container &&
+    props.gap &&
+    css`
+      margin-left: ${props.gap};
+    `}
+
+    ${(props) =>
+    !props.container &&
+    props.rowGap &&
+    css`
+      margin-top: ${props.rowGap};
+    `}
+
+    ${(props) =>
+    props.container &&
+    css`
+      display: flex;
+      flex-wrap: wrap;
+    `}
+
+    ${(props) =>
+    !props.container &&
+    props.span &&
+    css`
+      width: calc(${100 / (12 / props.span)}% - ${props.gap || '0rem'});
+    `}
+`
+
+const Grid = ({ children, container, gap, rowGap, span }: Props) => {
+  return (
+    <StyledContainer
+      container={container}
+      gap={gap}
+      rowGap={rowGap}
+      span={span}
+    >
+      {React.Children.map(children, (child) => {
+        if (container && React.isValidElement(child)) {
+          return React.cloneElement(child, { gap, rowGap })
+        }
+        return child
+      })}
+    </StyledContainer>
+  )
+}
+
+export default Grid
