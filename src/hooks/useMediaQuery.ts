fc0346218eb66e8bc6cd3f992c6f730c9cfe9514
Peralta Federico <peralta.federico.manuel@gmail.com>
2021-04-25 23:54:42 -0300

    remove deprecated callback

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -5,14 +5,18 @@ const useMediaQuery = (query: string) => {
 
   useEffect(() => {
     const media = window.matchMedia(query)
+
     if (media.matches !== matches) {
       setMatches(media.matches)
     }
+
     const listener = () => {
       setMatches(media.matches)
     }
-    media.addListener(listener)
-    return () => media.removeListener(listener)
+
+    media.addEventListener('change', listener)
+
+    return () => media.removeEventListener('change', listener)
   }, [matches, query])
 
   return matches
