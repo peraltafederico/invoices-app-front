dc8493e7a154aa98040285f72e3307810566ec7b
Peralta Federico <peralta.federico.manuel@gmail.com>
2021-10-17 21:13:07 -0300

    integrate update status invoice

--- src/schema/mutations/updateStateInvoice.ts
+++ src/schema/mutations/updateStateInvoice.ts
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client'
 
 export const UPDATE_STATE_INVOICE = gql`
-  mutation updateInvoice($id: Int!, $status: String) {
-    updateInvoice(id: $id, status: $status) {
+  mutation updateStatusInvoice($id: Int!, $status: String) {
+    updateStatusInvoice(id: $id, status: $status) {
       id
       status
     }
